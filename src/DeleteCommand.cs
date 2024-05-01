f842c41f694cac89186962eb718e0cb4d4796efd
Daniel Cazzulino <daniel@cazzulino.com>
2024-05-01 02:55:16 -0300

    Modernize by switching to Spectre.Console.Cli
    Remove the submodule, leverage the new CLI API.
    
    Fixes #23

---
+++ src/DeleteCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Spectre.Console.Cli;
+
+namespace Devlooped;
+
+public class DeleteCommand : AsyncCommand<UrlSettings>
+{
+    public override async Task<int> ExecuteAsync(CommandContext context, UrlSettings settings)
+    {
+        var input = settings.ToInputs();
+        if (HostProviders.GetProvider(input) is { } provider)
+        {
+            await provider.EraseCredentialAsync(input);
+            return 0;
+        }
+
+        return -1;
+    }
+}
